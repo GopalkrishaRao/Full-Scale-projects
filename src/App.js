3db3db910d2e33b668b7ba2a8a63e5206aa9f2c3
GK Rao <hrgkrao@gmail.com>
2023-01-09 22:53:53 +0530

    E commerce header footer and card added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import ComponetHome from './Components/ComponentHome';
 
 function App() {
   return (
    <>
     <h1 className="text-6xl">E-commerce Website</h1>
     <h1 className="text-xl">Loading .....</h1>
+
+    <ComponetHome/>
    </>
   );
 }
--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Card from './Card/Card';
+import ProductDescription from './IndividualProductDetails/ProductDescription';
 
-export default function home() {
+export default function Home() {
   return (
-    <div>home</div>
+    <>
+    <Card/>
+    <ProductDescription/>
+
+
+    </>
   )
 }

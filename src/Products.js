677988dd24fe465458e6fc41e63b374b8ea3d73a
GK Rao <hrgkrao@gmail.com>
2023-10-24 11:05:57 +0530

    ldl images

--- src/Products.js
+++ src/Products.js
@@ -3,10 +3,10 @@ import styled from "styled-components";
 import FilterSection from "./components/FilterSection";
 import Sort from "./components/Sort";
 import ProductList from "./components/ProductList";
-import { useFilterContext } from "./context/filter_context";
+// import { useFilterContext } from "./context/filter_context";
 
 const Products = () => {
-  const {filter_products} = useFilterContext;
+  // const {filter_products} = useFilterContext;
   // console.log(filter_products);
   return (
     <Wrapper>
